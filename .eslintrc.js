9aa59973fc2ca1ccddf20dd47b359f6a20bbd868
Wyatt Webb <wyatt.webb@modernhire.com>
2022-12-16 13:57:33 -0600

    Storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  overrides: [],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
+  overrides: [
+    {
+      // or whatever matches stories specified in .storybook/main.js
+      "files": ["*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+      "rules": {
+        // example of overriding a rule
+        //"storybook/hierarchy-separator": "error",
+        // example of disabling a rule
+        //"storybook/default-exports": "off",
+      }
+    }
+  ],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
     quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+    semi: ["error", "always"]
+  }
 };
